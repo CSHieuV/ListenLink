75c21f97d2c814b16f036cdcd35106f1fdecfd4d
Alby Alex <albysalex@gmail.com>
2023-03-25 19:18:20 -0400

    added twilio token

--- BackendFlask/ListenLink/app.py
+++ BackendFlask/ListenLink/app.py
@@ -7,8 +7,12 @@ from flask_sock import Sock, ConnectionClosed
 from twilio.twiml.voice_response import VoiceResponse, Start
 from twilio.rest import Client
 import vosk
+from twilio.jwt.access_token import AccessToken
+from twilio.jwt.access_token.grants import VoiceGrant
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 sock = Sock(app)
 twilio_client = Client()
 model = vosk.Model('../model')
@@ -28,6 +32,29 @@ def call():
     response.pause(length=60)
     print(f'Incoming call from {request.form["From"]}')
     return str(response), 200, {'Content-Type': 'text/xml'}
+
+@app.route('/twilio-token', methods=["GET"])
+def get_token():
+# required for all twilio access tokens
+# To set up environmental variables, see http://twil.io/secure
+    account_sid = os.environ['TWILIO_ACCOUNT_SID']
+    api_key = os.environ['TWILIO_API_KEY']
+    api_secret = os.environ['TWILIO_API_KEY_SECRET']
+
+    # required for Chat grants
+    identity = 'user'
+
+    # Create access token with credentials
+    token = AccessToken(account_sid, api_key, api_secret, identity=identity)
+
+    # Create a Voice grant and add to token
+    voice_grant = VoiceGrant(
+        incoming_allow=True,  # Optional: add to allow incoming calls
+    )
+    token.add_grant(voice_grant)
+
+    # Return token info as JSON
+    return str(token.to_jwt()), 200
 
 
 @sock.route('/stream')
@@ -56,8 +83,8 @@ def stream(ws):
 if __name__ == '__main__':
     from pyngrok import ngrok
     port = 5002
-
     public_url = ngrok.connect(port, bind_tls=True).public_url
+    print(public_url)
     number = twilio_client.incoming_phone_numbers.list()[0]
     number.update(voice_url=public_url + '/call')
     print(f'Waiting for calls on {number.phone_number}')
