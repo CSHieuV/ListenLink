e300d82bbaf43f1e7f4bfbb20c6388f1495538fa
Alby Alex <albysalex@gmail.com>
2023-03-25 23:51:59 -0400

    prepared files for Heroku Deployment

--- BackendFlask/ListenLink/app.py
+++ BackendFlask/ListenLink/app.py
@@ -85,9 +85,13 @@ def stream(ws):
 
 
 if __name__ == '__main__':
-    from pyngrok import ngrok
-    port = 5002
-    public_url = ngrok.connect(port, bind_tls=True).public_url
+
+    if os.environ == "HEROKU":
+        public_url = "listenlink.herokuapp.com"
+    else:
+        from pyngrok import ngrok
+        port = 5002
+        public_url = ngrok.connect(port, bind_tls=True).public_url
     print(public_url)
     number = twilio_client.incoming_phone_numbers.list()[0]
     number.update(voice_url=public_url + '/call')
