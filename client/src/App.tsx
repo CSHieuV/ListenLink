9de82aa072bafdb52f8fd6ad1bd0cc3ca7e8fe7b
Joel <joeltwh1999@gmail.com>
2023-03-26 02:48:29 -0400

    cloud build config, login page and updated twilio react flow

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,37 @@
-import { MantineProvider, Group, Text } from '@mantine/core';
+import { MantineProvider, Group, Text, Modal, Button } from '@mantine/core';
 import { ModalsProvider } from '@mantine/modals';
 import { observer } from 'mobx-react-lite';
 import { useStore } from './stores';
 import { useEffect } from 'react';
 import Navbar from './Navbar';
+import { useDisclosure } from '@mantine/hooks';
 
 const App = observer(() => {
+  const [opened, { open, close }] = useDisclosure(false);
   const appStore = useStore('appStore');
   useEffect(() => {
-    appStore.init();
+    appStore.init({ open, close });
   }, [])
 
   return (
     <MantineProvider withGlobalStyles withNormalizeCSS>
       <ModalsProvider>
         <Group>
-          <Navbar hasCall/>
+          <Navbar hasCall={appStore.hasCall}/>
           <Text>Welcome to Mantine!</Text>
         </Group>
+        <Modal
+          centered
+          opened={opened}
+          onClose={close}
+          withCloseButton={false}
+          closeOnClickOutside={false}
+          closeOnEscape={false}
+          title={`Incoming call from ${appStore.twilioIncomingPhoneNo}`}
+        >
+          <Button onClick={() => appStore.rejectCall(close)}>Cancel</Button>
+          <Button onClick={() => appStore.acceptCall(close)}>Confirm</Button>
+        </Modal>
       </ModalsProvider>
     </MantineProvider>
   )
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
 import { StoresProvider, stores } from "./stores";
+import { RouterProvider } from 'react-router-dom';
+import router from './router';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <StoresProvider value={stores}>
-      <App />
+      <RouterProvider router={router} />
     </StoresProvider>
   </React.StrictMode>,
 )
